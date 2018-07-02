6969d0f7f2dae7464790d5b1d1012f18c4089e1c
Kalalau <cantrellkalalau@gmail.com>
2018-07-02 19:01:01 -0400

    route params and route order

--- coltsteele_webdevbootcamp/app.js
+++ coltsteele_webdevbootcamp/app.js
@@ -13,6 +13,19 @@ app.get('/dog', (req, res) => {
   res.send('Meow!');
 });
 
+app.get('/r/:subRedditName', (req, res) => {
+  res.send(`Welcome to the ${req.params.subRedditName} subreddit`);
+});
+
+app.get('/r/:subRedditName/comments/:id/:title', (req, res) => {
+  console.log(req.params);
+  res.send('Welcome to the comments page');
+});
+
+app.get('*', (req, res) => {
+  res.send('You are a star!!');
+});
+
 app.listen(3000, () => {
   console.log('server listening on port 3000');
 });
