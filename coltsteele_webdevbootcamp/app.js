1127b8162c96906601dde3008fd3fae752b53d03
Kalalau <cantrellkalalau@gmail.com>
2018-07-02 21:27:02 -0400

    express views and templates

--- coltsteele_webdevbootcamp/app.js
+++ coltsteele_webdevbootcamp/app.js
@@ -1,34 +1,19 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-const ANIMAL_SOUNDS = {
-  pig: 'Oink!',
-  cow: 'Moo!',
-  dog: 'Woof Woof!',
-};
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Hi there, welcome to my page!')
+  res.render('home.pug');
 });
 
-app.get('/speak/:animal', (req, res) => {
-  const { animal } = req.params;
-  res.send(`The ${animal.toLowerCase()} says "${ANIMAL_SOUNDS[animal]}"`)
-});
-
-app.get('/repeat/:string/:multiplier', (req, res) => {
-  const { string, multiplier } = req.params;
-  let output = [];
-  for (let i = 1; i <= Number(multiplier); i++) {  
-    output.push(string);
-  }
-  res.send(output.join(' '));
-});
-
-app.get('*', (req, res) => {
-  res.send('Sorry, page not found...What are you doing with your life?');
+app.get('/fallinlovewith/:thing', (req, res) => {
+  const { thing } = req.params;
+  res.render('love.pug', {thing})
 });
 
 app.listen(3000, () => {
   console.log('server started')
-})
+});
