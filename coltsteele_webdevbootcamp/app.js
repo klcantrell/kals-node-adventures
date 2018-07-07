e5b68d156f3891a00edfea4a69feae459e0c39ac
Kalalau <cantrellkalalau@gmail.com>
2018-07-07 19:47:45 -0400

    added template partials

--- coltsteele_webdevbootcamp/app.js
+++ coltsteele_webdevbootcamp/app.js
@@ -6,12 +6,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('home.pug');
+  res.render('home');
 });
 
-app.get('/hobbies', (req, res) => {
-  const hobbies = ['coding', 'eating', 'gaming'];
-  res.render('hobbies.pug', {hobbies})
+app.get('/fallinlovewith/:thing', (req, res) => {
+  const { thing } = req.params;
+  res.render('love', {thing});
+});
+
+app.get('/posts', (req, res) => {
+  const posts = [
+    {title: 'coding', author: 'admin'},
+    {title: 'eating', author: 'kal'},
+    {title: 'gaming', author: 'cloud'},
+  ];
+  res.render('posts', {posts})
 });
 
 app.listen(3000, () => {
