2bf378866def7f8a89a1d5fbb8a4aa854670fdb6
Kalalau <cantrellkalalau@gmail.com>
2018-07-07 23:00:48 -0400

    practice with express post routes

--- coltsteele_webdevbootcamp/post-reqs-demo/app.js
+++ coltsteele_webdevbootcamp/post-reqs-demo/app.js
@@ -2,11 +2,24 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+let friends = ['Cloud', 'Squall', 'Vincent', 'Tifa'];
+
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/friends', (req, res) => {
+  res.render('friends', {friends});
+});
+
+app.post('/addfriend', (req, res) => {
+  const { newfriend } = req.body;
+  friends.push(newfriend);
+  res.redirect('/friends');
 });
 
 app.listen(3000, () => {
