525d1e2056e9d35fb0142cb436efe5b24767dca2
Kalalau <cantrellkalalau@gmail.com>
2018-07-09 16:12:58 -0400

    user can now search for movies and get results. also -> user can return to search route from results route

--- coltsteele_webdevbootcamp/movie_search_app/src/app.js
+++ coltsteele_webdevbootcamp/movie_search_app/src/app.js
@@ -6,10 +6,17 @@ app.set('view engine', 'pug');
 
 const baseUrl = 'http://www.omdbapi.com/?apikey=thewdb';
 
+app.get('/', (req, res) => {
+  res.render('search');
+});
+
 app.get('/results', (req, res) => {
-  request(`${baseUrl}&s=star`, (apiErr, apiRes, apiBody) => {
+  const { search } = req.query;
+  const url = `${baseUrl}&s=${search}`;
+  request(url, (apiErr, apiRes, apiBody) => {
     if (!apiErr && apiRes.statusCode === 200) {
-      res.render('results', {data: apiBody});
+      const data = JSON.parse(apiBody).Search;
+      res.render('results', {data});
     }
   });
 });
