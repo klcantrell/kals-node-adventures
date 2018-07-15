34878c219b4c66079cf2b0c3f3aa6ff41ccd2000
Kalalau <cantrellkalalau@gmail.com>
2018-07-14 21:00:17 -0400

    included semantic UI.  index view is loaded on default

--- coltsteele_webdevbootcamp/restful_blog/src/app.js
+++ coltsteele_webdevbootcamp/restful_blog/src/app.js
@@ -7,9 +7,17 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
 app.get('/blogs', (req, res) => {
-  res.status(200).render('index')
-})
+  db.Blog.all()
+    .then(blogs => {
+      res.render('index', {blogs});
+    })
+    .catch(err => res.send(err));
+});
 
 app.listen(3000, () => {
   console.log('Restul Blog server is running');
