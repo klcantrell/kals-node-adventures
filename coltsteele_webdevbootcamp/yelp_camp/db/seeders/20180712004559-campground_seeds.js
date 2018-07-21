43dfb0b2a0654e7bf5c74fa86e66ceac8ebbcf2c
Kalalau <cantrellkalalau@gmail.com>
2018-07-21 10:26:58 -0400

    basic styling for show page

--- coltsteele_webdevbootcamp/yelp_camp/db/seeders/20180712004559-campground_seeds.js
+++ coltsteele_webdevbootcamp/yelp_camp/db/seeders/20180712004559-campground_seeds.js
@@ -4,17 +4,17 @@ export default {
       {
         name: 'Salmon Creek',
         image: 'salmon-creek.jpg',
-        description: 'Good old fishy fun',
+        description: 'Good old fishy fun. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero tenetur sequi sed. Corporis sapiente quibusdam cumque distinctio delectus beatae? Eos necessitatibus culpa eveniet ab explicabo nostrum nam itaque fugiat ut.',
       },
       {
         name: 'Granite Hill',
         image: 'granite-hill.jpg',
-        description: 'Get ready for a rocky good time',
+        description: 'Get ready for a rocky good time. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero tenetur sequi sed. Corporis sapiente quibusdam cumque distinctio delectus beatae? Eos necessitatibus culpa eveniet ab explicabo nostrum nam itaque fugiat ut.',
       },
       {
         name: 'Mountain Goat\'s Rest',
         image: 'mountain-goats-rest.jpg',
-        description: 'Have a goat time!',
+        description: 'Have a goat time! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero tenetur sequi sed. Corporis sapiente quibusdam cumque distinctio delectus beatae? Eos necessitatibus culpa eveniet ab explicabo nostrum nam itaque fugiat ut.',
       }], {});
   },
 
