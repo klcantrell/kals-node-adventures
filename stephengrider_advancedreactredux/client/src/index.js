0c8a0027b346ff5f4e485fe9a1fe559cad82d612
Kalalau <cantrellkalalau@gmail.com>
2018-08-02 20:24:25 -0400

    basic redux setup

--- stephengrider_advancedreactredux/client/src/index.js
+++ stephengrider_advancedreactredux/client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Welcome from './components/Welcome';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <Router>
-    <App>
-      <Route path="/" exact component={Welcome} />
-    </App>
-  </Router>,
+  <Provider store={createStore(reducers, {})}>
+    <Router>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
+      </App>
+    </Router>
+  </Provider>,
   document.querySelector('#root'));
