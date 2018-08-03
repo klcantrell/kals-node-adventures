1c64989ecada31c801ba3e3fecd538c779d822a1
Kalalau <cantrellkalalau@gmail.com>
2018-08-02 20:05:44 -0400

    basic front end components set up

--- stephengrider_advancedreactredux/client/src/index.js
+++ stephengrider_advancedreactredux/client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Welcome from './components/Welcome';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Router>
+    <App>
+      <Route path="/" exact component={Welcome} />
+    </App>
+  </Router>,
+  document.querySelector('#root'));
