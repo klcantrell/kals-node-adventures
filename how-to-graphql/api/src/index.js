d184d8503dd9f31a77e888e8be50f6db32b7e15e
Kalalau <cantrellkalalau@gmail.com>
2018-08-27 22:06:29 -0400

    a simple mutation

--- how-to-graphql/api/src/index.js
+++ how-to-graphql/api/src/index.js
@@ -6,32 +6,62 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL',
 }];
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (root, args) => links.filter(l => l.id === args.id)[0]
   },
-  Link: { // can be omitted because the resolvers are "trivial"
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url,
-  }
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (root, args) => {
+      let idx;
+      const link = links.filter((l, i) => {
+        if (l.id === args.id) {
+          idx = i;
+          return true;
+        }
+        return false;
+      })[0];
+      const { id, ...newProps } = args;
+      const newLink = { ...link, ...newProps };
+      console.log(newLink);
+      links.splice(idx, 1, newLink);
+      return newLink;
+    },
+    deleteLink: (root, args) => {
+      let deletedLink;
+      const newLinks = links.filter(l => {
+        if (l.id === args.id) {
+          deletedLink = l;
+          return false;
+        }
+        return l.id !== args.id;
+      });
+      links = newLinks;
+      return deletedLink;
+    }
+  },
+// can be omitted because the resolvers are "trivial"
+  // Link: {
+  //   id: root => root.id,
+  //   description: root => root.description,
+  //   url: root => root.url,
+  // }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
