4b97630508593cc8df36098eacde66dddfe02b57
Kalalau <cantrellkalalau@gmail.com>
2018-09-02 20:16:43 -0400

    refactored apollo client config

--- how-to-graphql/client/src/index.js
+++ how-to-graphql/client/src/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { AUTH_TOKEN } from './constants';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-  request: operation => {
-    const token = localStorage.getItem(AUTH_TOKEN);
-    operation.setContext({
-      headers: {
-        authorization: token,
-      }
-    })
-  },
+});
+
+const authLink = setContext((reqest, previousContext) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      authorization: token,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
