0925dd13f50ab27b0fe20a9e5820e165c442f0b4
Kalalau <cantrellkalalau@gmail.com>
2018-09-21 22:26:27 -0400

    include lodash

--- andrewmead_complete/notes/app.js
+++ andrewmead_complete/notes/app.js
@@ -2,13 +2,16 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
+const _ = require('lodash');
 
-const user = os.userInfo();
+const filteredArray = _.uniq([1, 2, 2, 2, 4, 3, 'kalalau', 'kalalau']);
+console.log(filteredArray);
 
-console.log(user);
+// const user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}`, (err) => {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}, you are ${notes.age}`, (err) => {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
---
+++ andrewmead_complete/notes/notes.js
@@ -0,0 +1,11 @@
+console.log('starting notes.js');
+
+module.exports = {
+  addNote() {
+    console.log('note');
+    return 'new note';
+  },
+  add(a, b) {
+    return a + b;
+  },
+};
