2d5d9a2e0350e0217850b57529bb2ea96ea1a2bb
Kalalau <cantrellkalalau@gmail.com>
2018-09-23 20:21:48 -0400

    encode user input for URI component

--- andrewmead_complete/weather/app.js
+++ andrewmead_complete/weather/app.js
@@ -1,12 +1,42 @@
 const fetch = require('node-fetch');
+const yargs = require('yargs');
 const env = require('./env');
 
-const url = `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${process.env.API_KEY}`;
+const argv = yargs
+  .options({
+    address: {
+      describe: 'Address to fetch weather for',
+      alias: 'a',
+      string: true,
+      demand: true,
+    },
+  })
+  .help()
+  .alias('help', 'h').argv;
+
+const addressURIComponent = encodeURIComponent(argv.address);
+const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${addressURIComponent}&key=${
+  process.env.API_KEY
+}`;
 
 const getLocation = async () => {
-  const res = await fetch(url);
-  const { results: [ { geometry: { location } } ]  } = await res.json();
-  console.log(JSON.stringify(location, undefined, 2));
+  try {
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const {
+      results: [
+        {
+          geometry: { location },
+        },
+      ],
+    } = await res.json();
+    console.log(JSON.stringify(location, undefined, 2));
+  } catch (e) {
+    console.log(`Error is: ${e}`);
+  }
 };
 
 getLocation();
