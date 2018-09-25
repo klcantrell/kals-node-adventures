7e752c2d333da9e001928245f82077e261b68e21
Kalalau <cantrellkalalau@gmail.com>
2018-09-24 21:32:44 -0400

    add function for getting weather based on lat and lng

--- andrewmead_complete/weather/app.js
+++ andrewmead_complete/weather/app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const fetch = require('node-fetch');
 const env = require('./env');
 
 const geocode = require('./geocode/geocode');
@@ -15,10 +16,34 @@ const argv = yargs
   .help()
   .alias('help', 'h').argv;
 
+const getCurrentTemperature = async (coordinates, cb) => {
+  const url = `https://i34eo8f73h.execute-api.us-east-2.amazonaws.com/darksky/your-current-weather/${coordinates}`;
+  try {
+    const res = await fetch(url, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const data = await res.json();
+    if (data.error) {
+      cb(data.error);
+    } else if (res.ok && res.status === 200) {
+      cb(`${data.currently.temperature}\u00B0F`);
+    }
+  } catch (e) {
+    console.log('Unable to connect to Dark Sky servers');
+  }
+};
+
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, null, 2));
+    const coordinates = `${results.latitude},${results.longitude}`;
+    getCurrentTemperature(coordinates, (errorMessage, results) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(results);
+      }
+    });
   }
 });
