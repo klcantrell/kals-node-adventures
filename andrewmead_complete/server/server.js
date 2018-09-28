8014ab12f0c387077dbb6f08b7489715f14830d4
Kalalau <cantrellkalalau@gmail.com>
2018-09-27 21:43:53 -0400

    partials and helpers

--- andrewmead_complete/server/server.js
+++ andrewmead_complete/server/server.js
@@ -1,15 +1,39 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
+hbs.registerPartials(path.resolve('views/partials'));
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', `${log} \n`, err => {
+    err && console.log(err);
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance');
+});
+
 app.use(express.static(path.resolve('public')));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home', {
-    pageTitle: 'Home',
-    currentYear: new Date().getFullYear(),
+    pageTitle: 'Home Page',
     welcomeMessage: 'Hey there you!',
   });
 });
@@ -17,7 +41,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
