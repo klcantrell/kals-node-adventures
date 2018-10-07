798d53393ee3833d96943851ac4f7ebc22b73204
Kalalau <cantrellkalalau@gmail.com>
2018-10-07 18:10:56 -0400

    es6 excercise solutions

--- typescript_max/app.ts
+++ typescript_max/app.ts
@@ -186,39 +186,36 @@ function makeArray(...args: number[]) {
 // ES6 in TS exercises
 
 // Exercise 1 - Maybe use an Arrow Function?
-var double = function(value) {
+const double = (value: number): number => {
   return value * 2;
 };
 console.log(double(10));
 
 // Exercise 2 - If only we could provide some default values...
-var greet = function(name) {
-  if (name === undefined) {
-    name = 'Max';
-  }
+const greet = (name: string = 'Max'): void => {
   console.log('Hello, ' + name);
 };
 greet();
 greet('Anna');
 
 // Exercise 3 - Isn't there a shorter way to get all these Values?
-var numbers = [-3, 33, 38, 5];
-console.log(Math.min.apply(Math, numbers));
+const numbers: number[] = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
 
 // Exercise 4 - I have to think about Exercise 3 ...
-var newArray = [55, 20];
-Array.prototype.push.apply(newArray, numbers);
+const newArray: number[] = [55, 20];
+newArray.push(...numbers);
 console.log(newArray);
 
 // Exercise 5 - That's a well-constructed array.
-var testResults = [3.89, 2.99, 1.38];
-var result1 = testResults[0];
-var result2 = testResults[1];
-var result3 = testResults[2];
+const testResults: number[] = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults;
 console.log(result1, result2, result3);
 
 // Exercise 6 - And a well-constructed object!
-var scientist = { firstName: 'Will', experience: 12 };
-var firstName = scientist.firstName;
-var experience = scientist.experience;
+const scientist: { firstName: string; experience: number } = {
+  firstName: 'Will',
+  experience: 12,
+};
+const { firstName, experience } = scientist;
 console.log(firstName, experience);
