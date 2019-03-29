2b5ac7c7ce3f0264001dd8d671c178a2ac80b480
klcantrell <cantrellkalalau@gmail.com>
2019-03-28 21:54:03 -0400

    wesbos-advreact: add updateItem to types, resolvers, and UI

--- Advanced-React/sick-fits/backend/src/resolvers/Mutation.js
+++ Advanced-React/sick-fits/backend/src/resolvers/Mutation.js
@@ -8,6 +8,13 @@ const Mutations = {
 
     return item;
   },
+  async updateItem(parent, args, ctx, info) {
+    const { id, ...updates } = args;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: { id },
+    });
+  },
 };
 
 module.exports = Mutations;
--- Advanced-React/sick-fits/backend/src/resolvers/Query.js
+++ Advanced-React/sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,10 @@ const Query = {
     const items = await ctx.db.query.items();
     return items;
   },
+  async item(parent, args, ctx, info) {
+    const item = await ctx.db.query.item({ where: args.where });
+    return item;
+  },
 };
 
 module.exports = Query;
