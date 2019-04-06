96d8ef525c94cd8a1a5a08c10f99448b8b1a2b6c
klcantrell <cantrellkalalau@gmail.com>
2019-04-05 22:39:59 -0400

    wesbos-advreact: add query to get currently logged in user

--- Advanced-React/sick-fits/backend/src/resolvers/Query.js
+++ Advanced-React/sick-fits/backend/src/resolvers/Query.js
@@ -13,6 +13,19 @@ const Query = {
   async itemsConnection(parent, args, ctx, info) {
     return await ctx.db.query.itemsConnection({ where: args.where }, info);
   },
+  async me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
