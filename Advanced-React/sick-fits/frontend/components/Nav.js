acbf6763bbdf1eacd33d03ab01f5cb84dce03267
klcantrell <cantrellkalalau@gmail.com>
2019-04-16 21:00:06 -0400

    wesbos-advreact: add cart count w/ animation

--- Advanced-React/sick-fits/frontend/components/Nav.js
+++ Advanced-React/sick-fits/frontend/components/Nav.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
 import User from './User';
+import CartCount from './CartCount';
 import NavStyles from './styles/NavStyles';
 import Signout from '../components/Signout';
 
@@ -25,7 +26,17 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
