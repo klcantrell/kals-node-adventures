a053d81f05579e61cb5dd1d6c52e1a5e6d4e5bbc
klcantrell <cantrellkalalau@gmail.com>
2019-04-17 22:15:53 -0400

    wesbos-advreact: integrate downshift into autocomplete

--- Advanced-React/sick-fits/frontend/components/Search.js
+++ Advanced-React/sick-fits/frontend/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Downshift from 'downshift';
+import Downshift, { resetIdCounter } from 'downshift';
 import Router from 'next/router';
 import { ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -29,7 +29,6 @@ const AutoComplete = () => {
 
   const handleChange = debounce(async (e, client) => {
     setLoading(true);
-    console.log('Searching...');
     const res = await client.query({
       query: SEARCH_ITEMS_QUERY,
       variables: { searchTerm: e.target.value },
@@ -38,28 +37,65 @@ const AutoComplete = () => {
     setLoading(false);
   }, 350);
 
+  const routeToItem = item => {
+    Router.push({
+      pathname: '/item',
+      query: {
+        id: item.id,
+      },
+    });
+  };
+
+  resetIdCounter();
   return (
     <SearchStyles>
-      <div>
-        <ApolloConsumer>
-          {client => (
-            <input
-              type="search"
-              onChange={e => {
-                handleChange(e.nativeEvent, client);
-              }}
-            />
-          )}
-        </ApolloConsumer>
-        <DropDown>
-          {items.map(item => (
-            <DropDownItem>
-              <img width="50" src={item.image} alt={item.title} key={item.id} />
-              {item.title}
-            </DropDownItem>
-          ))}
-        </DropDown>
-      </div>
+      <Downshift
+        onChange={routeToItem}
+        itemToString={item => (item === null ? '' : item.title)}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+        }) => (
+          <div>
+            <ApolloConsumer>
+              {client => (
+                <input
+                  {...getInputProps({
+                    type: 'search',
+                    placeholder: 'Search For An Item',
+                    id: 'search',
+                    className: loading ? 'loading' : '',
+                    onChange: e => {
+                      handleChange(e.nativeEvent, client);
+                    },
+                  })}
+                />
+              )}
+            </ApolloConsumer>
+            {isOpen && (
+              <DropDown>
+                {items.map((item, idx) => (
+                  <DropDownItem
+                    {...getItemProps({ item })}
+                    key={item.id}
+                    highlighted={idx === highlightedIndex}
+                  >
+                    <img width="50" src={item.image} alt={item.title} />
+                    {item.title}
+                  </DropDownItem>
+                ))}
+                {!items.length && !items.loading && (
+                  <DropDownItem>Nothing found for {inputValue}</DropDownItem>
+                )}
+              </DropDown>
+            )}
+          </div>
+        )}
+      </Downshift>
     </SearchStyles>
   );
 };
