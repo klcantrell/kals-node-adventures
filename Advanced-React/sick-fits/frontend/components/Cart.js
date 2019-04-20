fe86aa5c21345efa6063fa08782023769c977cbc
klcantrell <cantrellkalalau@gmail.com>
2019-04-19 21:35:20 -0400

    wesbos-advreact: credit card processing with Stripe frontend

--- Advanced-React/sick-fits/frontend/components/Cart.js
+++ Advanced-React/sick-fits/frontend/components/Cart.js
@@ -10,6 +10,7 @@ import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query LOCAL_STATE_QUERY {
@@ -56,7 +57,9 @@ const Cart = () => {
             </ul>
             <footer>
               <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-              <SickButton>Checkout</SickButton>
+              <TakeMyMoney>
+                <SickButton>Checkout</SickButton>
+              </TakeMyMoney>
             </footer>
           </CartStyles>
         );
